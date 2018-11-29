724692fcf83f6f8627d2b785729bf8e27c7ae1de
James Kenny <james.m.kenny@gmail.com>
2018-11-29 11:03:51 +0000

    First

--- StringGeneratorDemo/Program.cs
+++ StringGeneratorDemo/Program.cs
@@ -10,6 +10,27 @@ namespace StringGeneratorDemo
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("starting...");
+
+      // Generate a random string the simple way.
+      Console.WriteLine(GenerateString(14));
+
+      Console.WriteLine("complete");
+      Console.ReadKey();
+    }
+
+    static string GenerateString(int iLen)
+    {
+      StringBuilder builder = new StringBuilder();
+      Random random = new Random();
+      char ch;
+      for (int i = 0; i < iLen; i++)
+      {
+        ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+        builder.Append(ch);
+      }
+
+      return builder.ToString();
     }
   }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is just a simple console app to generate random strings.
+
+At the moment I do it one way but I am going to add a number of ways to generate random strings for codes in C# in this project.
